3f3f6fcaefebeb26a2d416b8399b96beb02ce824
Welington <wzaunir@gmail.com>
2016-09-27 21:59:36 -0300

    Aula 7 - Configuração do Symphony

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response; 
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,13 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+    
+    /**
+     * @Route("/teste",name="teste")
+     */
+    public function testeAction(Request $request)
+    {
+        return new Response('Olá turma');
     }
 }
